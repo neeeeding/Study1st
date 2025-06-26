edc329fd9117fcfc1c07d005733d2fb77a198648
neeeeding <abc01096780866@gmail.com>
2025-06-26 22:48:41 +0900

    Fix : SaveManager

--- Assets/01Script/Manager/SaveManager.cs
+++ Assets/01Script/Manager/SaveManager.cs
@@ -1,7 +1,40 @@
-﻿namespace _01Script.Manager
+﻿using System;
+using _01Script.Save;
+using UnityEngine;
+
+namespace _01Script.Manager
 {
-    public class SaveManager
+    public class SaveManager : MonoBehaviour
     {
-        
+        [SerializeField]private GameSaveData _data; //저장되는 곳
+        private string _fileName = "SaveData";
+
+        [ContextMenu("R")]
+        private void R()
+        { 
+            PlayerPrefs.SetString(_fileName, "");
+        }
+        private void Awake()
+        {
+            if (PlayerPrefs.GetString(_fileName) != "") //안 비어 있다면
+            {
+                print("저장용 스크립트를 불러 왔습니다.");
+                string json = PlayerPrefs.GetString(_fileName);
+                _data = JsonUtility.FromJson<GameSaveData>(json);
+            }
+            else
+            {
+                print("저장용 스크립트를 생성 했습니다.");
+                _data = new GameSaveData();
+            }
+        }
+
+        private void OnApplicationQuit()
+        {
+            print("저장용 스크립트를 저장 했습니다.");
+            string json = JsonUtility.ToJson(_data);
+            PlayerPrefs.SetString(_fileName, json);
+            PlayerPrefs.Save();
+        }
     }
 }
